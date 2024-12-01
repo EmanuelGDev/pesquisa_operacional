7b7e647f0c5d541719865c812aa8a22702918ad3
Emanuel <emanuel.gui.santana@gmail.com>
2024-12-01 19:34:17 -0300

    adicionando classe alimento

---
+++ alimento.py
@@ -0,0 +1,17 @@
+class alimento:
+  
+  def __init__(self, proteina, caloria, carboidrato, gordura, sodio, vitaminaC, ferro, magnesio, calcio, preco):
+    self.proteina = proteina  
+    self.caloria = caloria  
+    self.carboidrato = carboidrato  
+    self.gordura = gordura  
+    self.sodio = sodio  
+    self.vitaminaC = vitaminaC  
+    self.ferro = ferro  
+    self.magnesio = magnesio  
+    self.calcio = calcio  
+    self.preco = preco 
+    
+  def __init__(self):
+    self
+    
